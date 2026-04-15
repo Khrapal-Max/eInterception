dbadf4ff0aaf5d6cb42362c3e37bc6d2903b535d
Khrapal Maksym <khrapal_m_g@outlook.com>
2026-04-15 16:52:44 +0300

    add RegistryInterceptionDivisionTests -> PASS

--- src/Domain/Entities/RegistryInterceptionDivision.cs
+++ src/Domain/Entities/RegistryInterceptionDivision.cs
@@ -32,7 +32,7 @@ public sealed class RegistryInterceptionDivision
     public static RegistryInterceptionDivision Create(string frequencyCode, string? divisionName = null)
     {
         var normalizedFrequencyCode = FrequencyCodeVo.Create(frequencyCode)
-            ?? throw new ArgumentException("Частота радіоперехоплення обов'язкова.", nameof(frequencyCode));
+            ?? throw new ArgumentNullException(nameof(frequencyCode), "Частота радіоперехоплення обов'язкова.");
 
         var normalizedDivisionName = DivisionNameVo.Create(divisionName);
 
@@ -47,8 +47,12 @@ public sealed class RegistryInterceptionDivision
     // -------------------------------------------------------------------------
     // Behaviour
     // -------------------------------------------------------------------------
-    public void Update(string? divisionName = null)
+    public void Update(string frequencyCode, string? divisionName = null)
     {
+        var normalizedFrequencyCode = FrequencyCodeVo.Create(frequencyCode)
+           ?? throw new ArgumentNullException(nameof(frequencyCode), "Частота радіоперехоплення обов'язкова.");
+
+        FrequencyCode = normalizedFrequencyCode;
         DivisionName = DivisionNameVo.Create(divisionName);
     }
 }
